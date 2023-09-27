70a11cbbf6114609fae6e700952520d8395b4b7a
德歪 郑 <zhang.tieying@qq.com>
2023-09-27 15:27:45 +0800

    试一下

--- wordbook.c
+++ wordbook.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-int equal(const char s1[],const char s2[]){printf("6");
-    int ifequal=0;
-    int i=0;
-    while(s1[i]==s2[i]&& s1[i]!='\0'&&s2[i]!='\0') i++;
-    if(s1[i]=='\0'&&s2[i]=='\0') ifequal =1;
-    return ifequal;
-}
 struct node{
     char Eng[20],Chi[10];
 };
@@ -16,41 +8,38 @@ int main(){
     struct node word[100];
     int key;
     int i=0;
-    B:
-    printf("1.添加单词\n2.查看单词\n3.复习\n");
+    while(1){
+    printf("1.add\n2.check\n3.review\n");
    
     scanf("%d",&key);
     switch (key)
     {
-    case 1: 
-        A:  printf("请输入汉语：\n");
+    case 1:while(1){
+            printf("Chinese:\n");
             scanf("%s",word[i].Chi);
-            printf("请输入英语：\n");
+            printf("English:\n");
             scanf("%s",word[i].Eng);
-            printf("继续摁1 结束摁2");
-            int a;scanf("%d",&a);;i++;
-            if(a==1) {
-               goto A;
-            }
-            if(a==2) goto B;
-            else break;
+            printf("1.continue;2.exist\n");
+            int a;scanf("%d",&a);i++;
+            if(a==2) { break; }
+            }       
     case 2:
-        for(int j=0;j<=i;j++){
+        for(int j=0;j<i;j++){
             printf("%s ",word[j].Chi);
             printf("%s\n",word[j].Eng);
         }        
-        goto B;
+        
     case 3:
-        for(int j=0;j<=i;j++){
-            printf("请输入");printf("%s",word[j].Chi);printf("的英文");
+        for(int j=0;j<i;j++){
+            printf("please input the English of ");printf("%s\n",word[j].Chi);
             char temword[20];
-            scanf("%s",word->Eng);
-            if(equal(temword,word[j].Eng))
+            scanf("%s",temword);
+            if(strcmp(temword,word[j].Eng))
                 printf("nb\n");
             else printf("sb\n");
         }
-        goto B;
+        
 
-    }
+    }}
  return 0;
 }
