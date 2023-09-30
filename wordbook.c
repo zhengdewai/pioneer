75ee7c6f45c89b51b36493ffa557124c0043113a
德歪 郑 <zhang.tieying@qq.com>
2023-09-30 16:50:29 +0800

    试一下

--- wordbook.c
+++ wordbook.c
@@ -6,7 +6,7 @@ struct node{
 };
 int main(){
     struct node word[100];
-    int key;
+    int key=0;
     int i=0;
     while(1){
     printf("1.add\n2.check\n3.review\n");
@@ -20,15 +20,15 @@ int main(){
             printf("English:\n");
             scanf("%s",word[i].Eng);
             printf("1.continue;2.exist\n");
-            int a;scanf("%d",&a);i++;
-            if(a==2) { break; }
+            int a=0;scanf("%d",&a);i++;
+            break;
             }       
     case 2:
         for(int j=0;j<i;j++){
             printf("%s ",word[j].Chi);
             printf("%s\n",word[j].Eng);
         }        
-        
+        break;
     case 3:
         for(int j=0;j<i;j++){
             printf("please input the English of ");printf("%s\n",word[j].Chi);
@@ -38,7 +38,8 @@ int main(){
                 printf("nb\n");
             else printf("sb\n");
         }
-        
+    default:
+        break;
 
     }}
  return 0;
